a8b4c6f2827ab2e84d663a623c61a5657c7f3261
Andrea Coi <andrea.coi@outlook.it>
2024-11-15 19:48:54 +0100

    modifica percorso assoluto con variabile ambiente

--- fake-polling.sh
+++ fake-polling.sh
@@ -2,8 +2,8 @@
 
 while true; do
   # Comando o script da eseguire ogni secondo
-  /home/andrea/sys/autostart/gestisci-periferiche/gestisci-tastiera-bt-laptop.sh
-  /home/andrea/sys/autostart/gestisci-periferiche/gestisci-mouse-bt-laptop.sh
+  $HOME/sys/autostart/gestisci-periferiche/gestisci-tastiera-bt-laptop.sh
+  $HOME/sys/autostart/gestisci-periferiche/gestisci-mouse-bt-laptop.sh
 
   # Pausa di 1 secondo
   sleep 1
