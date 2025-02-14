547da353ec8f4b6254ea92cd5363cd792e1f9c8b
Andrea Coi <andrea.coi@outlook.it>
2025-02-14 19:08:56 +0100

    cambia modalità di diagnosi della presenza della tastiera

--- gestisci-tastiera-bt-laptop.sh
+++ gestisci-tastiera-bt-laptop.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Nome o parte del nome della tastiera Bluetooth
-TASTIERA_BLUETOOTH_LOGITECH="MX MCHNCL M"
+MAC_TASTIERA_BLUETOOTH_LOGITECH="D8:8B:4D:EA:76:87"
 
 # Nome o parte del nome della tastiera del laptop - da xinput risulta standard come AT Translated Set 2 keyboard
 LAPTOP_KEYBOARD_NAME="AT Translated Set 2 keyboard"
 
 # Controlla se il dispositivo Bluetooth specificato è connesso
-is_keyboard_connected=$(bluetoothctl info | grep -q "Name: $TASTIERA_BLUETOOTH_LOGITECH" && echo "yes" || echo "no")
+is_keyboard_connected=$(bluetoothctl info $MAC_TASTIERA_BLUETOOTH_LOGITECH | grep "Connected" | awk '{print $2}')
 
 # Controlla lo stato della tastiera del laptop
 stato_tastiera_portatile=$(xinput list-props "$LAPTOP_KEYBOARD_NAME" | grep "Device Enabled" | awk '{print $4}')
 
 if [ "$is_keyboard_connected" == "yes" ]; then
-  if [ "$stato_tastiera_portatile" -eq 1 ]; then
+  if [ "$stato_tastiera_portatile" == "1" ]; then
     xinput disable "$LAPTOP_KEYBOARD_NAME"
   fi
 else
